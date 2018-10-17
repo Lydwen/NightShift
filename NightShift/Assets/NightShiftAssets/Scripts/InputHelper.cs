8a031a478f8b285af97f4d99f52813ac058a6935
Lydwen <tomlau94@aol.com>
2018-10-17 18:36:20 +0200

    Add gym scene, a simple movement script, and first character sprite

---
+++ NightShift/Assets/NightShiftAssets/Scripts/InputHelper.cs
@@ -0,0 +1,9 @@
+﻿namespace dallagnol.tom.nightshift
+{
+    public static class InputHelper
+    {
+        public static string HORIZONTAL_AXIS = "Horizontal";
+        public static string VERTICAL_AXIS = "Vertical";
+
+    }
+}
---
+++ NightShift/Assets/NightShiftAssets/Scripts/SimpleMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace dallagnol.tom.nightshift
+{
+    /// <summary>
+    ///     Component to move simply a transform
+    /// </summary>
+    public class SimpleMovement : MonoBehaviour
+    {
+        [SerializeField]
+        private float _speed = 0.5f;
+        [SerializeField]
+        private Transform _transformToMove;
+
+        // Update is called once per frame
+        void Update()
+        {
+            Vector3 movementToApply = Vector3.zero;
+            movementToApply += ApplyMovement(InputHelper.HORIZONTAL_AXIS, _transformToMove.right);
+            movementToApply += ApplyMovement(InputHelper.VERTICAL_AXIS, _transformToMove.forward);
+
+            _transformToMove.Translate(movementToApply, Space.Self);
+        }
+
+        private Vector3 ApplyMovement(string axis, Vector3 normalVector)
+        {
+            float axisValue = Input.GetAxis(axis);
+
+            if (axisValue != 0f)
+            {
+                return normalVector * axisValue * _speed;
+            }
+            return Vector3.zero;
+        }
+    }
+}
